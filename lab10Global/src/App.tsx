82d2d1b74d8bc9bcdd3393eb470921a57e10311f
Stanislav-Kovalchuk <147875939+Stasnislav-Kovalchuk@users.noreply.github.com>
2024-11-18 19:48:13 +0200

    start_working
    start_working

---
+++ lab10Global/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import Layout from "./components/layouts/Layout/Layout";
+import HomePage from "./components/pages/HomePage/HomePage";
+import CatalogPage from "./components/pages/CatalogPage/CatalogPage";
+import ItemPage from "./components/pages/ItemPage/ItemPage";
+import ServicePage from "./components/pages/ServicePage/ServicePage";
+
+function App() {
+    return (
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route
+                        index
+                        element={<HomePage />}
+                    />
+                    <Route path="catalog" element={<CatalogPage/>} />
+                    <Route path="catalog/:id" element={<ItemPage/>} />
+                    <Route path="service/:id" element={<ServicePage/>} />
+
+                </Route>
+            </Routes>
+
+
+    );
+}
+
+export default App;
